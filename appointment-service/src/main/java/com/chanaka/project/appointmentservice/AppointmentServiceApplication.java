fe84ee70fb418cfaf439024f22d0b99574b7b965
ChanakaVrtsa <chanakaroshane8332@gmail.com>
2021-02-14 12:13:06 +0530

    Modified Appointment

--- appointment-service/src/main/java/com/chanaka/project/appointmentservice/AppointmentServiceApplication.java
+++ appointment-service/src/main/java/com/chanaka/project/appointmentservice/AppointmentServiceApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableResourceServer
 @EntityScan(basePackages = "com.chanaka.project.commons.model")
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AppointmentServiceApplication {
 
 	public static void main(String[] args) {
--- appointment-service/src/main/java/com/chanaka/project/appointmentservice/Controller/AppointmentController.java
+++ appointment-service/src/main/java/com/chanaka/project/appointmentservice/Controller/AppointmentController.java
@@ -6,12 +6,14 @@ import com.chanaka.project.commons.model.Vehicle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "services/appointments")
+@PreAuthorize("hasAuthority('Role_customer')")
 public class AppointmentController {
 
     @Autowired
